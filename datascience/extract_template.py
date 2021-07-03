6451ac0285446fca145fe517ee7815d242c3dc0d
rickraul <rickraul01@gmail.com>
2021-07-03 15:43:56 +0530

    commented print statements

--- datascience/extract_template.py
+++ datascience/extract_template.py
@@ -5,11 +5,11 @@ import pandas as pd
 def xlsx_extractto_csv(filename,startrow,endrow,startcol,endcol,output_csv):
    file=filename
    data=pd.ExcelFile(file)
-   print(data.sheet_names)
+   #print(data.sheet_names)
    df=data.parse(data.sheet_names[0])#("Informatics")
-   print(df.info,"\n",df.head(10))
+   #print(df.info,"\n",df.head(10))
    df1=df.iloc[startrow:endrow,startcol:endcol]
-   print("\n\n",df1.head(18))
+   #print("\n\n",df1.head(18))
    df1.to_csv(output_csv)
 
 

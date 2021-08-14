0999c55c4096516938c04a4736044feea3b62f3e
HandidaT <htromep@gmail.com>
2021-08-14 03:20:12 -0700

    BR29 for ABMD

--- datascience/etl.py
+++ datascience/etl.py
@@ -25,6 +25,7 @@ def empirical(filename,sheet):
    return actual,Max,Min
    
 engine = sqlalchemy.create_engine('mysql+pymysql://root:@localhost:3306/AnalystData')
+engine2 = sqlalchemy.create_engine('mysql+pymysql://root:@localhost:3306/Data')
    
 def regression(filename,sheet):
    data1=pd.read_excel(filename,sheet_name=sheet)
@@ -33,12 +34,21 @@ def regression(filename,sheet):
    indx=index[data1[Str] == "Max "].tolist()[0]#data1[Str].strip()
    data=data1.iloc[indx-1:indx+2,16:18].values
    actual=data[0][1]
-   Max=data[1][1]#Max
-   Min=data[2][1]#min
+   #Max
+   Max=data[1][1]
+   #min
+   Min=data[2][1]
    d=data1.iloc[indx-1:indx,2:4].values[0][0]
    year="20"+d[2:]
    quarter=d[:2]
    return quarter,year,actual,Max,Min
+   
+def Data(filename,tablename,sheet):
+   data1=pd.read_excel(filename,sheet_name=sheet)
+   latestmonth = data1.iloc[[-1]]['Date'].values[0]
+   data2 = data1[data1["Date"] == latestmonth]
+   data3=data2.loc[:,['Date', 'FacilityType', 'BedSize', 'Region', 'Manufacturer', 'Ticker', 'Group', 'Therapy', 'Anatomy','SubAnatomy', 'ProductCategory', 'Quantity', 'AvgPrice', 'TotalSpend']]
+   data3.to_sql(name=tablename,con=engine2,if_exists='append')
 
 def print_df():
   print(csvdf)
@@ -51,10 +61,9 @@ def parse(filename,ticker,dfrowinx=0):
    f1=pd.read_excel(filename,sheet_name=None)
    flag1=0;er_count=0;emp_returnlen=3;reg_returnlen=5
    emp_arr=[];reg_arr=[];emp_type=[];reg_type=[]
+   pattern1=re.compile(r'Empirical Model');pattern2=re.compile(r'Regression Model');pattern3=re.compile(r'Data')
    for key in f1.keys():
       print(key)
-      pattern1=re.compile(r'Empirical Model')
-      pattern2=re.compile(r'Regression Model')
       for j in pattern1.finditer(key):
          if '-' in key:
             temp1=key.split('-')
@@ -82,19 +91,29 @@ def parse(filename,ticker,dfrowinx=0):
          reg_arr.append(forecast_actual)
          reg_arr.append(forecast_max)
          reg_arr.append(forecast_min)
+      for j in pattern3.finditer(key):
+         print(j)
+         temp1=filename.split(".")[0]
+         tablename=temp1.split('/')[1]
+         Data(filename,tablename,key)
+         print(tablename)
+      #er_count*2
    for i in range(er_count):
       if not emp_type:
+         print("########################################## type None ################################################################")
          Type=None
       else:
          print(emp_type)
          if emp_type[i]!=reg_type[i]:
+            print("########################################## types not equal ################################################################")
          else:
+            print("########################################## types equal ################################################################")
             Type=emp_type[i]
       csvdf.loc[dfrowinx]=[currentdate,ticker,Type,reg_arr[i],reg_arr[i+1],emp_arr[i],emp_arr[i+1],emp_arr[i+2],reg_arr[i+2],reg_arr[i+3],reg_arr[i+4]]
       csvdf.to_sql(name='outputdata',con=engine,index=False,if_exists='append')
       csvdf.to_csv('abmd.csv')
       dfrowinx+=1
-   #print_df()
+   print_df()
 
 
 
@@ -102,6 +121,7 @@ def parse(filename,ticker,dfrowinx=0):
 with zipfile.ZipFile("ETL Datasheet1.zip","r") as my_zip:
    csvdf = pd.DataFrame(columns=['Date', 'Ticker', 'Type','Quarter','Year','Estimated_Total_Sold','Estimated_Sold_Max'\
                           ,'Estimated_Sold_Min','Forecast_wo_SA_Actual','Forecast_wo_Max','Forecast_wo_Min'])
+   #t()
    dfrowinx=0
    for i in my_zip.namelist():
       my_zip.extract(i)
@@ -113,6 +133,7 @@ with zipfile.ZipFile("ETL Datasheet1.zip","r") as my_zip:
          ticker=i[j.start()+1:j.end()]
          print(ticker)
       currentdate=date.today().strftime("%Y/%m/%d")
+      #ticker= already initialized
       parse(i,ticker,dfrowinx)
 
 
